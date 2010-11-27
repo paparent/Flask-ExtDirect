3bd48e48f4e8ff3fbf34dff38848b49b113eabad
PA Parent <paparent@paparent.me>
2010-11-27 14:28:47 -0500

    Initial commit

---
+++ flaskext/extdirect.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.extdirect
+    ~~~~~~~~~~~~~~~~~~
+
+    Adds Ext.Direct support to Flask. Under development.
+
+    :copyright: (c) 2010 by PA Parent.
+    :license: MIT, see LICENSE for more details.
+"""
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Flask-ExtDirect
+---------------
+
+Adds Ext.Direct support to Flask. Under development.
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-ExtDirect>`_
+* `development version
+  <http://github.com/paparent/Flask-ExtDirect/zipball/master#egg=Flask-ExtDirect-dev>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-ExtDirect',
+    version='0.1',
+    url='http://github.com/paparent/Flask-ExtDirect/',
+    license='MIT',
+    author='PA Parent',
+    author_email='paparent@paparent.me',
+    description='Adds Ext.Direct support to Flask.',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
